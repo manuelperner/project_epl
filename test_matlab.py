f59b1aabca7d38046bce868f4526a26b1955dbcc
yanick dickbauer <yanick@dickbauer.at>
2017-01-28 16:19:40 +0100

    blb

--- test_matlab.py
+++ test_matlab.py
@@ -1,7 +1,4 @@
 import lib
-from tsp_heuristics import _kruskal
 
-matrix = lib.read_matrix_from_csv('matrix.csv')
-mst_graph = _kruskal(matrix)
-for edge in mst_graph:
-    print(edge)
+out = lib.run_matlab_script('Kruskal.m')
+print(out)
