98a58bac2e9cc5b07ad8ae8ca8dd059886ae77b3
manuelperner <manuelperner@gmail.com>
2017-01-30 21:20:25 +0100

    sorted tsp_heuristics functions

--- tsp_heuristics.py
+++ tsp_heuristics.py
@@ -3,80 +3,22 @@ from itertools import chain
 from lib import calc_route_length, create_matrix, write_matrix_to_csv, run_matlab_script
 from lib import print_matrix, print_route
 
-def nearest_neighbour(matrix):
-    """Returns a NN route (a list of indices: the first one is the start start index of the route, the last one is
-    the last vistited point in the route
-    """
-    all_routes = []
-    # calculate the nearest neighbour for each start point
-    for v in range(len(matrix)):
-        all_routes.append(__nearest_neighbour_fixed_start(matrix, v))
-    # return the best route of all:
-    return min(all_routes, key=lambda route: calc_route_length(route, matrix))
-    
+
 def first_come_first_serve(matrix):
     return list(range(len(matrix)))
-    
-def nearest_insertion(matrix):
-    tours = []
-    for i in range(len(matrix)):
-        tours.append(__nearest_insertion_fixed_start(matrix, i))
-    return min(tours, key=lambda k: calc_route_length(k, matrix))
-     
-def __nearest_insertion_fixed_start(matrix, start_point):
-    n = len(matrix)
-    tour = [start_point]
-    while len(tour) < n:
-        all_nearest = []
-        # find nearest:
-        for tour_point in tour:
-            possible_neighbours = [(i, val) for i, val in enumerate(matrix[tour_point]) if i not in tour]
-            nearest_to_point = min(possible_neighbours, key=lambda k: k[1])
-            all_nearest.append(nearest_to_point)
-        next_point = min(all_nearest, key= lambda i: i[1])
-        # find best insert position:
-        all_possible_tours = []
-        for pos in range(len(tour)):
-            possible_tour = tour[:pos+1] + [next_point[0]] + tour[pos+1:]
-            length = calc_route_length(possible_tour, matrix)
-            all_possible_tours.append( [possible_tour, length])
-        best_tour = min(all_possible_tours, key=lambda k: k[1])
-        tour = best_tour[0]
-    return tour
-    
-def cheapest_insertion(matrix):
-    tours = []
-    matrix = __calculate_weighted_matrix(matrix)
-    for i in range(len(matrix)):
-        tours.append(__nearest_insertion_fixed_start(matrix, i))
-    cheapest_route = min(tours, key=lambda k: calc_route_length(k, matrix))
-    cheapest_route_costs = calc_route_length(cheapest_route, matrix)
-    return cheapest_route, cheapest_route_costs
 
-def __calculate_weighted_matrix(matrix):
-    """modifies the already calculated distance matrix and returns a weighted distance matrix"""
-    n = len(matrix)
-    weighted_matrix = create_matrix(n, n)
-    for line in range(n):
-        for col in range(n):
-            if col%2 == 0:
-                weighted_matrix[line][col] = matrix[line][col] * 3
-            else:
-                weighted_matrix[line][col] = matrix[line][col]
-    return weighted_matrix
-    
-def mst_heuristic_matlab(matrix):
-    write_matrix_to_csv(matrix, 'matrix.csv')
-    route = run_matlab_script('Kruskal.m')
-    route = list(map(int, route.strip().split(',')))
-    return route
-    
 def mst_heuristic(matrix):
     try:
         return mst_heuristic_matlab(matrix)
     except:
         print('warning: matlab not installed')
         return mst_heuristic_python(matrix)
+
+def mst_heuristic_matlab(matrix):
+    write_matrix_to_csv(matrix, 'matrix.csv')
+    route = run_matlab_script('Kruskal.m')
+    route = list(map(int, route.strip().split(',')))
+    return route
     
 def mst_heuristic_python(matrix):
     # get mst graph
@@ -245,7 +187,18 @@ def _is_cycle_start_point(graph, start_point, visited):
             return False
     is_cycle = explore(start_point, None)
     return is_cycle
-    
+
+def nearest_neighbour(matrix):
+    """Returns a NN route (a list of indices: the first one is the start start index of the route, the last one is
+    the last vistited point in the route
+    """
+    all_routes = []
+    # calculate the nearest neighbour for each start point
+    for v in range(len(matrix)):
+        all_routes.append(__nearest_neighbour_fixed_start(matrix, v))
+    # return the best route of all:
+    return min(all_routes, key=lambda route: calc_route_length(route, matrix))
+
 def __nearest_neighbour_fixed_start(matrix, start):
     """Returns a NN route (a list of indices: the first one is the start start index of the route, the last one is
     the last vistited point in the route
@@ -258,7 +211,7 @@ def __nearest_neighbour_fixed_start(matrix, start):
         route.append(nearest)
         unvisited -= set([nearest])
     return route
-    
+
 def __find_nearest(distance_matrix, v, unvisited):
     """Searches for the point with the nearest distance from vertex v to another one within the unvisited"""
     assert len(unvisited) > 0
@@ -269,3 +222,51 @@ def __find_nearest(distance_matrix, v, unvisited):
         if distance_matrix[v][u] < distance_matrix[v][nearest]:
             nearest = u
     return nearest
+    
+def nearest_insertion(matrix):
+    tours = []
+    for i in range(len(matrix)):
+        tours.append(__nearest_insertion_fixed_start(matrix, i))
+    return min(tours, key=lambda k: calc_route_length(k, matrix))
+     
+def __nearest_insertion_fixed_start(matrix, start_point):
+    n = len(matrix)
+    tour = [start_point]
+    while len(tour) < n:
+        all_nearest = []
+        # find nearest:
+        for tour_point in tour:
+            possible_neighbours = [(i, val) for i, val in enumerate(matrix[tour_point]) if i not in tour]
+            nearest_to_point = min(possible_neighbours, key=lambda k: k[1])
+            all_nearest.append(nearest_to_point)
+        next_point = min(all_nearest, key= lambda i: i[1])
+        # find best insert position:
+        all_possible_tours = []
+        for pos in range(len(tour)):
+            possible_tour = tour[:pos+1] + [next_point[0]] + tour[pos+1:]
+            length = calc_route_length(possible_tour, matrix)
+            all_possible_tours.append( [possible_tour, length])
+        best_tour = min(all_possible_tours, key=lambda k: k[1])
+        tour = best_tour[0]
+    return tour
+    
+def cheapest_insertion(matrix):
+    tours = []
+    matrix = __calculate_weighted_matrix(matrix)
+    for i in range(len(matrix)):
+        tours.append(__nearest_insertion_fixed_start(matrix, i))
+    cheapest_route = min(tours, key=lambda k: calc_route_length(k, matrix))
+    cheapest_route_costs = calc_route_length(cheapest_route, matrix)
+    return cheapest_route, cheapest_route_costs
+
+def __calculate_weighted_matrix(matrix):
+    """modifies the already calculated distance matrix and returns a weighted distance matrix"""
+    n = len(matrix)
+    weighted_matrix = create_matrix(n, n)
+    for line in range(n):
+        for col in range(n):
+            if col%2 == 0:
+                weighted_matrix[line][col] = matrix[line][col] * 3
+            else:
+                weighted_matrix[line][col] = matrix[line][col]
+    return weighted_matrix
